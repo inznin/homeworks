f78a78e7c3554e79f25e5b1ba9b6b98e62bbd796
nznin <201469497+inznin@users.noreply.github.com>
2025-08-12 17:28:14 +0330

    Update task numpy.py

--- task numpy.py
+++ task numpy.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# --- ساخت تصویر ساده ---
+# --- creat image ---
+
 img = np.zeros((256,256))
 rr, cc = np.ogrid[:256, :256]
 circle = (rr - 128)**2 + (cc - 128)**2 < 50**2
@@ -20,7 +21,7 @@ def conv2d(img, kernel):
 img += conv2d(np.eye(256)[150][:,np.newaxis], kernel)
 img = (img - img.min()) / (img.max() - img.min())
 
-# --- FFT و ماسک ---
+# --- FFT  ---
 F_true = np.fft.fft2(img)
 mag = np.abs(F_true)
 rows, cols = img.shape
@@ -29,14 +30,15 @@ mask = (X - cols//2)**2 + (Y - rows//2)**2 < 80**2
 F_meas = F_true * mask
 mag_meas = np.abs(F_meas)
 
-# --- بازسازی ساده ---
+
 F_est = mag_meas * np.exp(1j * np.angle(np.fft.fft2(np.random.rand(*img.shape))))
 for _ in range(30):
     img_est = np.real(np.fft.ifft2(F_est))
     img_est[img_est<0] = 0
     F_est = mag_meas * np.exp(1j * np.angle(np.fft.fft2(img_est)))
 
-# --- نمایش ---
+# --- show ---
+
 plt.subplot(1,3,1)
 plt.imshow(img, cmap='gray'); plt.title('Ground Truth'); plt.axis('off')
 plt.subplot(1,3,2)
